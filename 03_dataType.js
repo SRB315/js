9698aa1287fd3f8ac71df26a865d2b501b570557
Smruti Ranjan Behera <srbsmruti123@gmail.com>
2024-02-12 13:11:42 +0000

    dataType Summery

---
+++ 03_dataType.js
@@ -0,0 +1,27 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
+
+const bigNumber = 23232323224424425646664565654654666466564n
+console.log(typeof bigNumber)
+
+const heros = ["saktiman", "naagarj", "doga" ];
+let myObj={
+    name: "smruti",
+    age: 23,
+}
+
+
+const myFuncation=function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFuncation);

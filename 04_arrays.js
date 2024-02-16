50a7a2da82906be9b45269a1d710cd4c140ea2f7
Smruti Ranjan Behera <srbsmruti123@gmail.com>
2024-02-16 11:17:22 +0000

    Hi

---
+++ 04_arrays.js
@@ -0,0 +1,23 @@
+//Arrays
+
+const myArr = [0,1,2,3,4,5,6,7]
+const myHeros = ["sakrtiman", "spiderman"]
+
+// console.log(myArr[1]);
+
+//Array Methnods
+// myArr.push(6)
+// myArr.push(7)
+// myArr.unshift(9)
+// myArr.shift()
+
+const newArr = myArr.join()
+console.log(typeof myArr);
+console.log(typeof newArr);
+
+//slice 
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
